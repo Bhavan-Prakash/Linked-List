b0112e72005bf454b49c96ffa90a8fd00e224ea8
Bhavan-Prakash <bhavanofficial2004@gmail.com>
2026-02-25 20:05:00 +0530

    25 feb

---
+++ singlyLinkedList/leetCode/Q83.java
@@ -0,0 +1,34 @@
+//Q83. Remove Duplicates from Sorted List
+
+
+
+package singlyLinkedList.leetCode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }else{
+            ListNode temp = head;
+            while(temp.next != null){
+                if(temp.val == temp.next.val){
+                    temp.next = temp.next.next;
+                }else{
+                    temp= temp.next;
+                }
+            }
+            // System.out.println(temp.val);
+            return head;
+        }
+    }
+}
