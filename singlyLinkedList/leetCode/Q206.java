f5a6e0fe4346c378384c9ae1f871e6b7b80d5785
Bhavan-Prakash <bhavanofficial2004@gmail.com>
2026-02-26 13:00:49 +0530

    26 feb

---
+++ singlyLinkedList/leetCode/Q206.java
@@ -0,0 +1,36 @@
+//package singlyLinkedList.leetCode;
+//
+//Q206 : Given the head of a singly linked list, reverse the list, and return the reversed list.
+//
+//
+///**
+// * Definition for singly-linked list.
+// * public class ListNode {
+// *     int val;
+// *     ListNode next;
+// *     ListNode() {}
+// *     ListNode(int val) { this.val = val; }
+// *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+// * }
+// */
+//class Solution {
+//    public ListNode reverseList(ListNode head) {
+//
+//        if(head == null){
+//            return null;
+//        }else{
+//            ListNode prev = null;
+//            ListNode current = head;
+//            while(current != null){
+//                ListNode nxt = current.next;
+//                current.next = prev;
+//                prev = current;
+//                current = nxt;
+//            }
+//            return prev;
+//        }
+//
+//    }
+//
+//
+//}
